e1583e171b14655f06e4a34bdcfa1384a4f757ee
Nirajkumargupta12527 <136949160+Nirajkumargupta12527@users.noreply.github.com>
2024-04-11 18:19:53 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"  //to use js code as new versions
+// alert(3+3)  this will give us error , used in browser
+console.log(3+3)
+console.log("niraj")
+
+let name="niraj"
+let age =21
+let islooged=true
+
+console.log(typeof "niraj")
+console.log(typeof age)
+console.log(typeof 21)
+
+// we write bigint for big numbers 
+// in simple int we can write at most number 2 to power 53
---
+++ basic/variable.js
@@ -0,0 +1,5 @@
+const accountId="12527879"
+var accountEmail="niraj.anisg@gmail.com"
+let accountpass="125278"
+accountCity="khaira"
+console.table([accountEmail,accountCity,accountId,accountpass])
