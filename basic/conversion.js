7bca99d465b241150b94ded6312b354868df96dd
Nirajkumargupta12527 <136949160+Nirajkumargupta12527@users.noreply.github.com>
2024-04-11 18:53:30 +0000

    some datatypes comnversion

---
+++ basic/conversion.js
@@ -0,0 +1,18 @@
+let score="33"
+let scoreinnumber=Number(score)
+console.log(scoreinnumber)
+console.log(typeof scoreinnumber);
+ 
+// 33abc -> val=NaN,number ; 33 ->number ; null-> val=0 ,number ; undefined ->NaN , undefined
+
+let bool=""
+let conv=Boolean(bool)
+console.log(conv);
+console.log(typeof conv);
+
+// ""->false ; ggvkjk->true ;1->true ;0->false
+
+let num=879789
+let str=String(num)
+console.log(str);
+console.log(typeof str);
