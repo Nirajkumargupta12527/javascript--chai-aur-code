a33882b36326234a022c215af78d43ce0a399a78
Nirajkumargupta12527 <136949160+Nirajkumargupta12527@users.noreply.github.com>
2024-04-15 06:11:05 +0000

    chage

--- basic/datatypes2.js
+++ basic/datatypes2.js
@@ -2,3 +2,28 @@ const id=Symbol('123')
 const anotherid=Symbol('123')
 console.log(id==anotherid)
 console.log(id);
+
+/***********************************************************/
+
+// stacks and heap
+// stack[primitive datatype , pass by value ya phir assan sabdo me ye copy bna ke bhejta hain ]
+//  heap ek non primitive datatype hain jisme ki reference se pass hota hain ...... so agr hm refernce me change kremnge to wo sab jgh reflect hoga 
+
+// let name="niraj kumar gupta"
+
+//  anoter =name
+// another="anish"
+// console.log(name);
+// console.log(another);
+
+//object bna lete hian 
+// isme refernce me change kiya to sabme change visible hoga 
+let user={
+    email:"niraj.2003@hotmail.com",
+    pass:"123456",
+}
+let user2=user;
+user2.email="anish.98765@hotmail.com";  //aise bhi access kiya ja skta hian isme 
+
+console.log(user.email);
+console.log(user2.email);
