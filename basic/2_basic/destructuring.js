899c75009b366e1e715c3943415a9f11df4db055
Nirajkumargupta12527 <136949160+Nirajkumargupta12527@users.noreply.github.com>
2024-05-01 08:44:18 +0000

    change

---
+++ basic/2_basic/destructuring.js
@@ -0,0 +1,12 @@
+const course={
+    coursename:"niraj12234",
+    courseteacher:"anish",
+    fee:9990,
+}
+//  this is caleed destructuring of an object
+const{courseteacher:name}=course
+console.log(name);
+
+// {} this eans destructuring
+// JSON format 
+// we write json format as {} as this .........in this the keys are also written as string and accesed as string 
